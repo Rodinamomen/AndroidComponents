edf76f0e349153801fad69753f768bf0ed463a09
Rodinamomen <Rodinamobark3@gmail.com>
2026-02-21 13:05:46 +0200

    Add explicit intent  within the app itself

--- app/src/main/java/com/bosta/androidcomponents/MainActivity.kt
+++ app/src/main/java/com/bosta/androidcomponents/MainActivity.kt
@@ -1,16 +1,16 @@
 package com.bosta.androidcomponents
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.bosta.androidcomponents.intent.SecondActivity
 import com.bosta.androidcomponents.ui.theme.AndroidComponentsTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +19,17 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             AndroidComponentsTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Column(modifier = Modifier.fillMaxSize()) {
+                    // Explicit Intent
+                    Button(onClick = {
+                        Intent(applicationContext, SecondActivity::class.java).also {
+                            startActivity(it)
+                        }
+                    }) {
+                        Text(text = "Go to Second Activity")
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    AndroidComponentsTheme {
-        Greeting("Android")
     }
 }
---
+++ app/src/main/java/com/bosta/androidcomponents/intent/SecondActivity.kt
@@ -0,0 +1,27 @@
+package com.bosta.androidcomponents.intent
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+
+class SecondActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+                Text(text = "Second Activity", color = Color.Red)
+            }
+        }
+    }
+}
